f2fb09c0bad8381f7d1d7d7f268e88c02373e9ee
ryanfox <fox.ryan.t@gmail.com>
2011-01-14 21:36:39 -0800

    Added menu system

--- hourlogger.rb
+++ hourlogger.rb
@@ -1,29 +1,41 @@
 class HourLogger
+    
+    def help
+        puts "Start new (m)onth, (l)oad existing month, (s)ave current month,
+              (c)reate new project, add (h)ours to existing project,
+              (p)rint project, print (a)ll projects, (h)elp, or (q)uit?"
+    end
 
-    def initialize
+    def addMonth
         puts "Enter month:"
-        @month = gets.chomp.downcase
+        @month = gets.chomp!.downcase
         @projects = []
     end
 
-    def addProject(projName)
-        @projects << [projName.downcase, 0] unless @projects.assoc(projName)
+    def addProject
+        puts "Enter project name:"
+        projName = gets.chomp!.downcase
+        @projects << [projName, 0] unless @projects.assoc(projName)
     end
 
     def addHours
         puts "Enter project name:"
-        name = gets.chomp.downcase
+        name = gets.chomp!.downcase
         puts "Enter hours:"
         hours = gets.to_f
         @projects.assoc(name)[1] += hours
     end
 
-    def print(project)
-        proj = @projects.assoc(project)
+    def print
+        puts "Enter project name:"
+        projName = gets.chomp!.downcase
+        proj = @projects.assoc(projName)
+        puts @month << ":"
         puts proj[0] + ": " + proj[1].to_s + " hours"
     end
 
     def printAllProjects
+        puts @month <<":"
         @projects.each {|proj| puts proj[0] + ": " + proj[1].to_s + " hours"}
     end
 
@@ -37,11 +49,33 @@ class HourLogger
 
     def read
         puts "Enter month to load:"
-        month = gets.chomp.downcase
+        month = gets.chomp!.downcase
         file = File.open(month)
         @projects = Marshal.load(file)
         @month = month
         puts month << " loaded"
+    end
+
+    # Display help first time
+    help
+
+    # Main loop
+    @quit = false
+    until @quit
+        
+        input = gets.chomp!.downcase
+        case input
+            when "m" then addMonth
+            when "l" then read
+            when "s" then save
+            when "c" then addProject
+            when "h" then addHours
+            when "p" then print
+            when "a" then printAllProjects
+            when "h" then help
+            when "q" then quit = true
+            else puts "Invalid input"
+        end
     end
 
 end
