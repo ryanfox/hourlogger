bd560d2fd6ecea8dc2a181fc6638a8347c208497
ryanfox <fox.ryan.t@gmail.com>
2011-01-15 12:59:25 -0800

    testing complete

--- hourlogger.rb
+++ hourlogger.rb
@@ -6,89 +6,146 @@ class HourLogger
     
     def self.help
         print "Start new (m)onth, (l)oad existing month, (s)ave current month,
-          (c)reate new project, add (h)ours to existing project,
+          (c)reate new project, (a)dd hours to existing project,
           (p)rint project, print all p(r)ojects, print all m(o)nths,
           (h)elp, or (q)uit? "
     end
 
     def self.addMonth
-        print "Enter month: "
-        @month = gets.chomp!.downcase
-        @projects = []
+        done = false
+        while done == false
+            print "Enter month: "
+            @month = gets.chomp!.downcase
+            @projects = []
+            if @month.length > 0
+                done = true
+            end
+        end
     end
 
     def self.addProject
-        print "Enter project name: "
-        projName = gets.chomp!.downcase
-        @projects << [projName, 0] unless @projects.assoc(projName)
+        if @month
+            done = false
+            while done == false
+                print "Enter project name: "
+                projName = gets.chomp!.downcase
+                if projName.length > 0
+                    done = true
+                end
+            end
+            @projects << [projName, 0] unless @projects.assoc(projName)
+        else
+            puts "Need to load a month first"
+        end
     end
 
     def self.addHours
+        if @month.nil?
+            puts "Need to load a month first"
+            return
+        elsif @projects.length == 0
+            puts "No projects to add to"
+            return
+        end
+
         print "Enter project name: "
         name = gets.chomp!.downcase
-        print "Enter hours:"
+        if @projects.assoc(name).nil?
+            raise NameError
+        end
+        print "Enter hours: "
         hours = gets.to_f
         @projects.assoc(name)[1] += hours
+    rescue NameError
+        puts "No such project"
+        retry
     end
 
     def self.printProject
+        if @month.nil?
+            puts "Need to load a month first"
+            return
+        end
         print "Enter project name: "
         projName = gets.chomp!.downcase
+        if @projects.assoc(projName).nil?
+            raise nameError
+        end
         proj = @projects.assoc(projName)
-        puts @month << ":"
+        puts @month + ":"
         puts proj[0] + ": " + proj[1].to_s + " hours"
+    rescue NameError
+        puts "No such project"
+        retry
     end
 
     def self.printAllProjects
-        puts @month <<":"
+        if @month.nil?
+            puts "Need to load a month first"
+            return
+        end
+        puts @month + ":"
+        if @projects.length == 0
+            puts "No projects in this month"
+        end
         @projects.each {|proj| puts proj[0] + ": " + proj[1].to_s + " hours"}
     end
 
     def self.printAllMonths
-        Dir["*.bak"].each {|month| puts month}
+        Dir["*.bak"].each {|month| puts month[0..-5]}
     end
 
     def self.save
-        marshalDump = Marshal.dump(@projects)
-        file = File.new(@month.downcase + ".bak", 'w')
-        file << marshalDump
-        file.close
-        puts @month << " saved"
-    end
-
-    def self.read
-        print "Enter month to load: "
-        month = gets.chomp!.downcase
-        file = File.open(month + ".bak")
-        @projects = Marshal.load(file)
-        @month = month
-        puts month << " loaded"
-    end
-
-    # Display help first time
-    help
-
-    # Main loop
-    @quit = false
-    until @quit
-    
-        print "\n"
-        print "? "        
-        input = gets.chomp!.downcase
-        case input
-            when "m" then addMonth
-            when "l" then read
-            when "s" then save
-            when "c" then addProject
-            when "h" then addHours
-            when "p" then printProject
-            when "r" then printAllProjects
-            when "o" then printAllMonths
-            when "h" then help
-            when "q" then @quit = true
-            else puts "Invalid input"
+        if @month
+            marshalDump = Marshal.dump(@projects)
+            file = File.new(@month.downcase + ".bak", 'w')
+            file << marshalDump
+            file.close
+            puts @month + " saved"
+        else
+            puts "Need to load a month first"
         end
     end
 
+    def self.read
+        begin
+            print "Enter month to load: "
+            month = gets.chomp!.downcase
+            file = File.open(month + ".bak")
+            @projects = Marshal.load(file)
+            @month = month
+            puts month + " loaded"
+        rescue
+            puts "No such month"
+            retry
+        end
+    end
+
+    if __FILE__ == $0
+        # Display help first time
+        help
+
+        # Main loop
+        @quit = false
+        until @quit
+    
+            print "\n"
+            print "? "        
+            input = gets.chomp!.downcase
+            case input
+                when "m" then addMonth
+                when "l" then read
+                when "s" then save
+                when "c" then addProject
+                when "a" then addHours
+                when "p" then printProject
+                when "r" then printAllProjects
+                when "o" then printAllMonths
+                when "h" then help
+                when "q" then @quit = true
+                else puts "Invalid input"
+            end
+        end
+    end
 end
 
